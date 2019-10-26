30184e025991fb5432d9b0b160916da8c91a71b6
Aidan Kelly <it.is.aidan@gmail.com>
2019-10-26 15:27:11 -0600

    Usernames fully implemented. Needs duplicate checking. Needs gracefully exiting.

--- basic_server.py
+++ basic_server.py
@@ -5,27 +5,36 @@ import threading
 #basic socket setup
 OUR_IP = '127.0.0.1'
 OUR_PORT = 1234
-
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((OUR_IP, OUR_PORT))
 server_socket.listen(5)
 
+#dictionary to map addresses to usernames
+ADDRESS_TO_NAME = {}
+CLIENTS = []
+
 print(f"[*] Listening on {OUR_IP}:{OUR_PORT}")
 
-#vars for number of acks, and number of connections
-ackNum = 0
-numConnection = 0
-clients = []
 
+#this function sends a message to all connections
 def broadcast(client_socket, message):
+
+    #see if the message is from the server
     if(client_socket == 'SERVER'):
-        for c in clients:
-            c.send(bytes(message, 'utf-8'))
-    else:
-        print("IN USER BIT")
-        for c in clients:
-            if c.getpeername() != client_socket.getpeername():
+
+        #if so send to all connected clients
+        for c in CLIENTS:
+            if c.getpeername() in ADDRESS_TO_NAME:
                 c.send(bytes(message, 'utf-8'))
+
+    #check if message is from connection
+    else:
+
+        #send to all connections that are not the sender
+        for c in CLIENTS:
+            if c.getpeername() != client_socket.getpeername():
+                messageToSend = f"{ADDRESS_TO_NAME[client_socket.getpeername()]}> " + message
+                c.send(bytes(messageToSend, 'utf-8'))
 
 
 #function to send and receive data to client
@@ -35,18 +44,17 @@ def handleClient(client_socket):
         message = client_socket.recv(1024).decode('utf-8')
         print(message)
 
+        #checks for format of message. 
         tester = message.split('~')
 
+        #see if we were sent a username
         if(tester[0] == 'NAME'):
+            ADDRESS_TO_NAME[client_socket.getpeername()] = tester[1]
             broadcast("SERVER", f"SERVER> {tester[1]} joined the chat.")
-
+            
+        #if not, broadcast the message
         else:
             broadcast(client_socket, message)
-
-                    ###
-                    ### TODO need to add the sender's name in from of message.
-                    ###      ex: <NAME>: Message goes here.
-                    ###
 
 
 #main server loop
@@ -54,9 +62,8 @@ while True:
 
     #accepts one connection for testing purposes
     client, addr = server_socket.accept()
-    numConnection +=1
     print(f"[*] New connection from {addr[0]}:{addr[1]}")
-    clients.append(client)
+    CLIENTS.append(client)
 
     #spawn new thread to handle client
     handleClientThread = threading.Thread(target=handleClient, args=(client,))
